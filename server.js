d37cbb385b24954239f54eb1f57d3f1e68526368
Peggy <64912576+pwangy@users.noreply.github.com>
2021-01-11 19:51:21 +0100

    eo video

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 import crypto from 'crypto'
+import bcrypt from 'bcrypt-nodejs'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/auth"
 mongoose.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true})
@@ -11,23 +12,35 @@ mongoose.Promise = Promise
 const User = mongoose.model('User', {
   name: {
     type: String,
-
+    unique: true
   },
   email: {
-    type: ,
+    type: String,
+    unique: true
   },
   password: {
     type: String,
+    required: true
   },
   accessToken: {
-
-  },
+    type: String, 
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
 })
 
+// middleware to authenticate a user
+const authenticateUser = async (req, res, next) => {
+  const user = await User.findOne({accessToken: req.header('Authorization')})
+  if (user) {
+    req.user = user
+    next()
+  } else {
+    res.status(401).json({loggedOut: true})
+  } 
+}
 
 const port = process.env.PORT || 8085
 const app = express()
-
 
 // this week we learn about CORS: Cross-Origin Resource Sharing
 app.use(cors())
@@ -38,8 +51,30 @@ app.get('/', (req, res) => {
   res.send('Coding a-long with Van!')
 })
 
+app.post('/users', async (req, res) => {
+  try {
+    const {name, email, password} = req.body
+    // DO NOT STORE PLAINTEXT PASSWORDS
+    const user = new User({name, email, password: bcrypt.hashSync(password)})
+    user.save()
+    res.status(201).json({id: user._id, accessToken: user.accessToken})
+  } catch (err) {
+    res.status(400).json({message: 'Could not create user', errors: err.errors})
+  }
+})
+
+app.get('/secrets', authenticateUser)
 app.get('/secrets', (req, res) => {
   res.json({secret: 'Super secret message.'})
+})
+
+app.post('/sessions', async (req, res) => {
+  const user = await User.findOne({email: req.body.email})
+  if (user && bcrypt.compareSync(req.body.password, user.password)) {
+    res.json({userId: user._id, accessToken: user.accessToken})
+  } else {
+    res.json({notFound: true})
+  }
 })
 
 // start server
